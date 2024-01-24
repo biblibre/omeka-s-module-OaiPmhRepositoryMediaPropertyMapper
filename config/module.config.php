8c59a5858b10954759502e36ba91706d9a8dc703
Julian Maurice <julian.maurice@biblibre.com>
2024-01-24 11:06:14 +0100

    Add translator configuration

--- config/module.config.php
+++ config/module.config.php
@@ -3,4 +3,14 @@
 namespace OaiPmhRepositoryMediaPropertyMapper;
 
 return [
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type' => 'gettext',
+                'base_dir' => dirname(__DIR__) . '/language',
+                'pattern' => '%s.mo',
+                'text_domain' => null,
+            ],
+        ],
+    ],
 ];
